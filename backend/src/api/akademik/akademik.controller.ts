a3fe3c27d3b37456c179c7d37c1f26dc4e0ea3da
muhamadSyahid <muhamadsyahid2016@gmail.com>
2025-11-26 15:03:39 +0700

    feat: add akademik api

---
+++ backend/src/api/akademik/akademik.controller.ts
@@ -0,0 +1,33 @@
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Query,
+  UseInterceptors,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AkademikService } from './akademik.service';
+import { AkademikTotalArrayDto } from './dto/akademik-total-array.dto';
+import { AuthGuard } from '../../common/guards/auth.guard';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { UserRole } from '../../constants/roles.constants';
+import { Roles } from '../../common/decorators/roles.decorator';
+
+@UseInterceptors(ClassSerializerInterceptor)
+@Controller('v1/mahasiswa')
+@UseGuards(AuthGuard, RolesGuard)
+@Roles(UserRole.DATACORE_ADMIN, UserRole.DATAVIEW_INTERNAL)
+@ApiTags('Akademik')
+export class AkademikController {
+  constructor(private readonly akademikService: AkademikService) {}
+
+  @Get('tipe-tes-masuk')
+  async getTipeTesMasuk(
+    @Query('angkatan') angkatan?: number,
+    @Query('prodi') prodi?: string,
+    @Query('kelas') kelas?: string,
+  ): Promise<AkademikTotalArrayDto> {
+    return this.akademikService.getTipeTesMasukData(angkatan, prodi, kelas);
+  }
+}
