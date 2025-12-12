4d8283919158123e2600c0a48c51561edc185b89
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-12-12 23:50:24 +0700

    feat(frontend): initial new vue + vite + shadcn setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+// TODO: Nanti kita bisa tambahkan komponen Global Toast/Notification (Toaster) dari shadcn di sini
+</script>
+
+<template>
+    <RouterView />
+</template>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-9xl text-primary">
+        Halaman <span class="text-secondary">Dummy</span>
+    </div>
+</template>
